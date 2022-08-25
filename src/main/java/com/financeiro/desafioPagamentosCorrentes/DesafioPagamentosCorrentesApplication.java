61c52bf6e3da984eaaf9b1b7584beb40c2208ce6
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-08-25 00:29:39 -0300

    preparação do ambiente de desenvolvimento do projeto desafio modulo 5

---
+++ src/main/java/com/financeiro/desafioPagamentosCorrentes/DesafioPagamentosCorrentesApplication.java
@@ -0,0 +1,13 @@
+package com.financeiro.desafioPagamentosCorrentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPagamentosCorrentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPagamentosCorrentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financeiro/desafioPagamentosCorrentes/DesafioPagamentosCorrentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeiro.desafioPagamentosCorrentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPagamentosCorrentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
